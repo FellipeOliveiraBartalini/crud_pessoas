79085a5b7e6e64f7195c3dba70915084475b88fa
FellipeOliveiraBartalini <fellipeoliveiranobre@hotmail.com>
2022-08-30 11:17:51 -0300

    Add Header and Home components

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import style from './Header.module.scss';
 
 export default function Header() {
+    const navigate = useNavigate();
+
     return (
         <header className={style.header}>
+            <button className={style.btnVoltar} onClick={() => navigate(-1)}>{'< Voltar'}</button>
             <h1>CRUD de Pessoas</h1>
+            <ul>
+                <li><Link to='/adicionarPessoa'>Create Pessoa</Link></li>
+            </ul>
         </header>
     );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import ListaPessoas from 'components/listaPessoas';
 import style from './Home.module.scss';
 
 export default function Home() {
     return (
-        <section>Section</section>
+        <section>
+            <h2>Lista de Pessoas cadastradas!</h2>
+            <ListaPessoas />
+        </section>
     );
 }
